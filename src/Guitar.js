53ebfc81a2d7bc7991840be64f950d7503fc956e
Nate Williams <nate@yogotech.com>
2019-05-21 15:55:41 -0600

    Update from the git repo referenced in the blog article

--- src/Guitar.js
+++ src/Guitar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Guitar = props => {
-  return React.createElement("div", {}, [
-  React.createElement("h1", {}, props.make),
-  React.createElement("h2", {}, props.model),
-  ]);
+export const Guitar = props => {
+  return (
+    <div>
+      <h1>{props.make} </h1>
+      <h2>{props.model}</h2>
+    </div>
+  );
 };
+
 export default Guitar;

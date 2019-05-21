df15a03a08cbef87a31c1c1677eff27203b47fd0
Nate Williams <nate@yogotech.com>
2019-05-21 15:19:01 -0600

    Very simple project based on blog at https://medium.com/@trivediu/getting-started-with-parcel-js-and-react-c83f19d89756

---
+++ src/App.js
@@ -0,0 +1,23 @@
+class App extends React.Component {
+  handleTitleClick() {
+    alert("You Clicked the Title!");
+  }
+render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "My First Parcel React App!"
+      ),
+      React.createElement(Guitar, {
+        make: "Fender",
+        model: "Stratocaster",
+      }),
+      React.createElement(Guitar, {
+        make: "Gibson",
+        model: "Les Paul",
+      }),
+    ]);
+  }
+}
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Guitar.js
@@ -0,0 +1,7 @@
+const Guitar = props => {
+  return React.createElement("div", {}, [
+  React.createElement("h1", {}, props.make),
+  React.createElement("h2", {}, props.model),
+  ]);
+};
+export default Guitar;
